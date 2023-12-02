266bfb03bdfee8af907e5ba53bb3606279bc22ac
Shadman <shadman.alizada@gmail.com>
2023-12-02 23:45:01 +0400

    chore: moved server to backend folder

--- backend/src/dbConnection.js
+++ backend/src/dbConnection.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');
 
 exports.connection = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     database: process.env.DB_DATABASE,
 });
